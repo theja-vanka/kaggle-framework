8f23223366c7d5f07e622ecdac436478dfa4a9f5
Krishnatheja Vanka <theja.vanka@gmail.com>
2021-07-25 16:37:27 +0530

    Add find_lr

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,5 +1,6 @@
 from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
 from helpers.config import Config
+import math
 
 
 # Torch convention
@@ -19,3 +20,44 @@ def init_logger(log_file=Config.OUTPUT_DIR+'train.log'):
     logger.addHandler(handler1)
     logger.addHandler(handler2)
     return logger
+
+
+def find_lr(model, train_loader, loss_fn, optimizer, init_value=1e-8, final_value=10.0):
+    number_in_epoch = len(train_loader) - 1
+    update_step = (final_value / init_value) ** (1 / number_in_epoch)
+    lr = init_value
+    optimizer.params_groups[0]["lr"] = lr
+    best_loss = 0.0
+    batch_num = 0
+    losses = []
+    log_lrs = []
+    for data in train_loader:
+        batch_num += 1
+        inputs, labels = data
+        inputs, labels = inputs, labels
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = loss_fn(outputs, labels)
+
+        # Crash if loss explodes
+        if batch_num > 1 and loss > 4 * best_loss:
+            return log_lrs[10:-5], losses[10:-5]
+
+        # Record the best loss
+        if loss < best_loss or batch_num == 1:
+            best_loss = loss
+
+        # Store the values
+
+        losses.append(loss)
+        log_lrs.append(math.log10(lr))
+
+        # Do the backward pass and optimize
+        loss.backward()
+        optimizer.step()
+
+        # Update the lr for the next step and store
+
+        lr *= update_step
+        optimizer.param_groups[0]["lr"] = lr
+    return log_lrs[10:-5], losses[10:-5]
